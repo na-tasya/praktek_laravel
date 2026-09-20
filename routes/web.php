b20a3cf7b67311ab3ef25e55d7bef21ad914c7f9
Natasya Azrielia Pachsya <natasyaazrielia22@gmail.com>
2026-09-20 22:45:27 +0700

    Praktek 2 Menambahkan route about

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return 'Pos Barokah Mart adalah toko yang menyediakan berbagai kebutuhan sehari-hari dengan harga terjangkau dan pelayanan yang ramah.';
+});
